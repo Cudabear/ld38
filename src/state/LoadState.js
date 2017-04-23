22f0d265d1623dd28015c6bd8eced18b46a5bf97
Ryan Graef <Ryan Graef>
2017-04-23 14:47:00 -0500

    stuff is hard

--- src/state/LoadState.js
+++ src/state/LoadState.js
@@ -53,7 +53,7 @@ LoadState.prototype = {
         }, this);
 
         console.log('kicking off the game');
-        game.state.start('StartupState');
+        game.state.start('selectHero');
     },
 
     update: function(){
